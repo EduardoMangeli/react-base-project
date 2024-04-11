276d7c4ba769a838a02c70317c2d0e0c43e0e62f
MARCELO SAGGIO DE MOURA <202307164186@alunos.ibmec.edu.br>
2024-04-11 11:10:32 -0300

    mudando header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,9 @@ import { Top } from "./Style";
 
 const Header = () => (
   <Top>
-    <Link to="/">Home</Link>
-    <Link to="/fotos">Fotos</Link>
-    <Link to="/atletas">Atletas</Link>
+    <Link to="/dashboard">DashBoard</Link>
+    <Link to="/servicos">Serviços</Link>
+    <Link to="/perfil">Perfil</Link>
     
     { window.sessionStorage.getItem('accessToken')
     ? <Link to="/logout">Logout</Link>
