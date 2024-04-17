6215b54a02acef1b13eb8814e027a7811e825252
mlutegar <mlutegar@gmail.com>
2024-04-17 13:17:17 -0300

    criação das páginas

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Login from "./pages/Login";
 import Logout from "./pages/Logout";
 import { auth } from "./config/Firebase";
 import { onAuthStateChanged } from "firebase/auth";
-import Atletas from "./pages/Atletas";
+import Atletas from "./pages/Servicos";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
