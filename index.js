84652ea0f8d514f9b98682a85423573f84a46b11
Victor Hwang <135401467+victorhwang3@users.noreply.github.com>
2024-04-17 23:42:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Fotos from "./pages/Fotos";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import { auth } from "./config/Firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import Atletas from "./pages/Atletas";
+import Freddie from "./pages/FreddieMercury";
+import Clinica from "./pages/Clinica";
+import Modelagem from "./pages/Modelagem";
+import Radiosinoviortese from "./pages/Radiosinoviortese";
+import SegmentacaoeQuantificacao from "./pages/SegmentacaoeQuantificacao";
+import DosimetriaPreClinica from "./pages/DosimetriaPreClinica";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+onAuthStateChanged(auth, (user)=> {
+  if (user) {
+    window.sessionStorage.setItem("accessToken", user.accessToken);
+  } else {
+    window.sessionStorage.removeItem("accessToken");
+  }
+});
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/freddie" element={<Freddie/>} />
+        <Route path="/fotos" element={<Fotos/>} />
+        <Route path="/atletas" element={<Atletas/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/logout" element={<Logout/>} />
+        <Route path="/clinica" element={<Clinica/>} />
+        <Route path="/modelagem" element={<Modelagem/>} />
+        <Route path="/radiosinoviortese" element={<Radiosinoviortese/>} />
+        <Route path="/SegmentacaoeQuantificacao" element={<SegmentacaoeQuantificacao/>} />
+        <Route path="/dosimetriapreclinica" element={<DosimetriaPreClinica/>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
