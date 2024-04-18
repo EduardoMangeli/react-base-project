d47ce04e980542ed01bcc7d88e081014a75dcae8
Victor Hwang <135401467+victorhwang3@users.noreply.github.com>
2024-04-17 23:46:35 -0300

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { Top } from "./Style";
+
+const Header = () => (
+  <Top>
+    <Link to="/">Home</Link>
+    <Link to="/clinica">Dosimetria Clinica</Link>
+    <Link to="/atletas">Atletas</Link>
+    <Link to="/modelagem">Modelagem Computacional</Link>
+    <Link to="/radiosinoviortese">Radiosinoviortese</Link>
+    <Link to="/segmentacaoequantificacao">Segmentacao e Quantificacao</Link>
+    <Link to="/dosimetriapreclinica">Dosimetria Pre Clinica</Link>
+    
+    { window.sessionStorage.getItem('accessToken')
+    ? <Link to="/logout">Logout</Link>
+    : <Link to="/login">Login</Link>
+    }
+    
+  </Top>
+)
+
+export default Header;
