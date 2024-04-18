15a6428d599362c94307857c970559a1fde33ec6
artic <migsveiga@gmail.com>
2024-04-17 21:28:00 -0300

    Adição de conteudo na pag About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,29 @@ const About = () => {
 
     return (
         <Base>
-            <h1>Quem Somos</h1>
+            <div id="cabeçalho_about">
+                <h1>Quem Somos</h1>
+            </div>
+
+            <div id="body_about">
+                <h2>Planos de Anuncio</h2>
+                <p>Divulgue o seu negocio no passeio carioca</p>
+
+                <h3>Plano Bronze</h3>
+                <p>Contaremos a historia do seu estabelecimento e colocaremos em destaque do nosso mapa</p>
+
+                <h3>Plano Ouro</h3>
+                <p> Plano Bronze + inclusão do “Viva essa experiencia”, com um link para a sua rede social ou um QR code, que podera incluir um desconto ou um premio para o turista ou morador da cidade</p>
+
+                <h3>Plano Diamante</h3>
+                <p> Plano Bronze + Plano Prata + inclusão do seu estabelecimento na gameficacao do aplicativo, com possibilidade do turista realizar checkin presencial no local e divulgar em suas redes socias </p>
+
+                <h4>Plano Bronze + Plano Prata + Plano Ouro + inclusão do seu estabelecimento em um dos passeios presenciais guiados</h4>
+
+                <p>Consulte valores e tire duvidas</p>
+
+                <button> <a href="Gameficacao.jsx">Saiba Mais</a></button>
+            </div>
         </Base>
     )
   }
