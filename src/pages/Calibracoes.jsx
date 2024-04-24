3bf9cba4be747c5c6c3790910e5d63432acb55cd
MARCELO SAGGIO <dev.marcelosaggio@gmail.com>
2024-04-24 20:54:55 -0300

    pagina de calibrações adicionada

---
+++ src/pages/Calibracoes.jsx
@@ -0,0 +1,55 @@
+import { onAuthStateChanged } from "firebase/auth"
+import Base from "./Base"
+import { auth } from "../config/Firebase";
+import { useEffect } from "react";
+import Secao from "../components/Secao/Secao";
+import dadosCalibracoes from "../data/calibracoes.json";
+import Tabela from "../components/Tabela/Tabela";
+
+const Calibracoes = () => {
+
+  /* useEffect(()=> {
+    onAuthStateChanged(auth, (user)=> {
+      if (user) {
+        window.sessionStorage.setItem("accessToken", user.accessToken);
+      } else {
+        window.sessionStorage.removeItem("accessToken");
+      }
+    })
+  },[]) */
+
+
+  return (
+    <Base>
+    <h1>Calibrações</h1>
+                        <div>
+                            <Tabela
+                                nome="Calibrações"
+                                tipo="calibracao"
+                                linha={
+                                    <>
+                                        {dadosCalibracoes.map(
+                                            (calibracao) => (
+                                                <tr>
+                                                    <td>{calibracao.id}</td>
+                                                    <td>{calibracao.nome}</td>
+                                                    <td>{calibracao.usuario}</td>
+                                                    <td>{calibracao.isotopo}</td>
+                                                    <td>{calibracao.imagem}</td>
+                                                    <td>{calibracao.date}</td>
+                                                </tr>
+                                            ))}
+                                    </>
+                                }
+                            />
+
+                            <button>Voltar</button>
+                            <button>Avançar</button>
+                        </div>
+                    
+                  
+    </Base>
+  )
+}
+
+export default Calibracoes
