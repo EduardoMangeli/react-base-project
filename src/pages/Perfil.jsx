4c7e8f6a8cb7418124ffc106ccdc4c17d5a79155
mlutegar <mlutegar@gmail.com>
2024-04-25 10:28:56 -0300

    testes

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,41 +1,80 @@
 import Base from "./Base"
 import TestCard from "../components/TestCard/TestCard";
+import {useState} from "react";
 import dadosSolicitacoes from "../data/solicitacoes.json";
 
 const Calibracoes = () => {
+    const [contador, setContador] = useState(0);
+    // O useState é um hook que permite adicionar estado a um componente funcional do React.
+    // O valor dentro do useState é o valor inicial do estado.
+    // O setContador é uma função que atualiza o estado.
+    const [idade, setIdade] = useState(21);
+
+    const maisUm = () => {
+        setContador(contador + 1);
+    }
+
+    const menosUm = () => {
+        setContador(contador - 1);
+    }
+
+    const mudaIdade = () => {
+        setIdade(40);
+    }
+
+    const [solicitacoes , setSolicitacoes] = useState(dadosSolicitacoes);
+
+    const filtra = (entrada) => {
+        setSolicitacoes(dadosSolicitacoes.filter(
+            (ele) => ele.cliente.includes(entrada)
+        ));
+    }
+
+
   return (
-    <Base>
-      <h1>
-     Aplicação React Base
-      </h1>
-        {dadosSolicitacoes.map(
-            (solicitacao) => (
-                <TestCard
-                    id={solicitacao.id}
-                    cliente={solicitacao.client}
-                    tipo_exame={solicitacao.description}
-                    data={solicitacao.date}
-                    status={solicitacao.status}
-                />
-            )
-        )}
+      <Base>
+          <h1>
+              Aplicação React Base
+          </h1>
 
-        <TestCard
-            id="1"
-            cliente="Clinica dosimetria"
-            tipo_exame="Clinic Dosimetries"
-            data="01/01/2021"
-            status="A"
-        />
+          <div>
 
-        <TestCard
-            id="2"
-            cliente="Clinica dosimetria"
-            tipo_exame="Preclinic Dosimetris"
-            data="01/01/2021"
-            status="A"
-        />
-    </Base>
+              <input type="text" onChange={(e) => filtra(e.target.value)}/>
+
+              {solicitacoes.map(
+                  (solicitacao) => (
+                      <TestCard
+                          id={solicitacao.id}
+                          cliente={solicitacao.cliente}
+                          tipo_exame={solicitacao.description}
+                          data={solicitacao.date}
+                          status={solicitacao.status}
+                      />
+                  )
+              )}
+          </div>
+
+          <div>
+              <h1>Contador: {contador}</h1>
+              <button onClick={maisUm}>+1</button>
+              <button onClick={menosUm}>-1</button>
+              <button onClick={() => setContador(0)}>zerar</button>
+          </div>
+
+          <div>
+              <h1>Idade: {idade}</h1>
+              <button onClick={mudaIdade}>Mudar Idade</button>
+              <input value={idade} onChange={(e) => setIdade(e.target.value)}/>
+              {/*
+            OnChange é um evento que será chamado toda vez que o valor do input for alterado.
+            O (e) é o evento que está sendo passado para a função. Qualquer nome que você colocar no lugar do (e) funcionará.
+            O e.target é o elemento que está sendo alterado. Que no caso é o input.
+            O e.target.value é o valor do elemento que está sendo alterado.
+            O nome desse processo é Data Binding.
+        */}
+          </div>
+
+      </Base>
   )
 }
 
