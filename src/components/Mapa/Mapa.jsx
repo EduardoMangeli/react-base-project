a6d81cb9319130c97b47b9430a71305f1f10860c
Zecademonho <jg@assis.net.br>
2024-04-30 14:42:23 -0300

    adicionando marker no mapa, e modificando um pouco o popup do mapa(ainda nao completo), comecando os pontosInteresse.json

--- src/components/Mapa/Mapa.jsx
+++ src/components/Mapa/Mapa.jsx
@@ -1,21 +1,42 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
 import "./Style.css";
 import "leaflet/dist/leaflet.css";
+
+import pontosInteresse from '../../data/pontosInteresse.json';
+
+const customIconUrl = process.env.PUBLIC_URL + 'imagens/icons/marker-icon.png';
+
+const customIcon = L.icon({
+  iconUrl: customIconUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
 
 function Mapa() {
     return (
         <div className="map-container">
             <div className="map-inner-container">
-                <MapContainer center={[-22.951804, -43.210760]} zoom={13} style={{ height: '100%', width: '100%' }}>
+                <MapContainer center={[-22.951804, -43.210760]} zoom={11} style={{ height: '100%', width: '100%' }}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={[-22.951804, -43.210760]}>
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
+                    {pontosInteresse.map(e => (
+                      <Marker key={e.id} position={[e.latitude, e.longitude]} icon={customIcon}>
+                        <Popup className="custom-popup">
+                            <div className="popup-content">
+                                <div className="popup-text">
+                                    <h3>{e.nome}</h3>
+                                    <p>{e.popupContent}</p>
+                                </div>
+                                <img src={`${process.env.PUBLIC_URL}${e.imagem}`}/>
+                            </div>
                         </Popup>
-                    </Marker>
+                      </Marker>
+                    ))}
                 </MapContainer>
             </div>
         </div>
