f26c7ef4540e7d9b41d2d172b3e49f475f12e853
CAIO DE SOUZA DE MEDEIROS <202203369008@alunos.ibmec.edu.br>
2024-05-09 11:31:38 -0300

    update

---
+++ src/components/DadosPaciente/Style.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+const MenuDadosPaciente = styled.div 
+    `
+.div-bar {
+
+}
+.div-rotulo {
+    margin: 30px;
+}
+.menu-dados {
+    margin: 10px;
+}
+.div-botoes {
+    display: flex;
+    height: 2rem;
+    justify-content: center;
+}
+h2 {
+    font-size: 90%;
+    text-align: center;
+    margin: 0 0px;
+}
+.btn-bar{
+    font-size: 90%;
+    cursor: pointer;
+    border-style: none;
+    margin: 0 5px;
+    min-width: 13rem;
+    padding: .40rem;
+    text-align: center;
+    font-weight: lighter;
+    text-decoration: none;
+    color: white;
+    background-color: var(--detalhes);
+}
+.btn-bar.selecionado{
+    padding-bottom: 1.62rem;
+    background-color: var(--botoes);
+}
+@media (max-width: 900px){
+    .btn-bar{
+        font-size: 60%;
+        min-width: 7rem;
+    h2 {
+            font-size: 80%;
+        }
+    }
+  }
+@media (max-width: 800px){
+    .btn-bar{
+        padding: .2rem;
+        font-size: 60%;
+        min-width: 4rem;
+    }
+    .btn-bar.selecionado{
+        padding-bottom: 2rem;
+    }
+    h2 {
+        font-size: 70%;
+    }
+  }
+
+`
+export {MenuDadosPaciente}
---
+++ src/pages/Teste.jsx
@@ -0,0 +1,14 @@
+import Base from "./Base";
+import { DadosPaciente } from "../components/DadosPaciente/DadosPaciente";
+
+const Menu = (props) => 
+(
+    <>
+    <Base>
+        <DadosPaciente>
+        </DadosPaciente>
+    </Base>
+    </>
+)
+
+export default Menu;
