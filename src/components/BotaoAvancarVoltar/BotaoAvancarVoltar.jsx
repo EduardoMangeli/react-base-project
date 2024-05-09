641089dc4722b2627bda1d5a7d0634dad7b669e7
tadeu tovar <tadeutovar@hotmail.com>
2024-05-08 22:05:33 -0300

    botao avancar e voltar feito

---
+++ src/components/BotaoAvancarVoltar/BotaoAvancarVoltar.jsx
@@ -0,0 +1,10 @@
+import { BotaoAvancarVoltar } from "./Style";
+
+const BotaoAvancarVoltar = () => (
+  <BotaoAvancarVoltar>
+    <a href="#" className="previous round">&#8249; Voltar</a>
+    <a href="#" className="next round">Avançar &#8250;</a>
+  </BotaoAvancarVoltar>
+)
+
+export default BotaoAvancarVoltar;
