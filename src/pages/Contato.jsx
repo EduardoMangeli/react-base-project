a1a7011a1b074fde0798a34a2c6d7823f262fd1b
Ewerton Peixoto de Alencar Arrais <142873014+ewerside@users.noreply.github.com>
2024-05-16 00:28:18 -0300

    Finalizando a página de contato geral

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import Banner from "../components/Banner/Banner";
 import Botao from "../components/Botao/Botao";
 import Base from "./Base"
@@ -5,10 +6,15 @@ import { Link } from "react-router-dom";
 import { StyleAnun } from "../components/Anunciante/StyeAnunciante";
 
 const Contato = () => {
-
+    const { t } = useTranslation();
     return (
         <Base>
-            <Banner titulo={"Contato"} imagem={""}/>
+            <Banner titulo={t("Contato")} imagem={""}/>
+            <div className="adTexto">
+                <h1>{t('Fale Conosco')}</h1> 
+                <p>{t('Temos um compromisso em oferecer o melhor atendimento e garantir que sua experiência explorando o Rio de Janeiro seja inesquecível. Se você tem dúvidas, sugestões ou precisa de mais informações, não hesite em nos contactar. Nossa equipe está pronta para ajudá-lo a descobrir todas as maravilhas que o Rio tem a oferecer.')}</p>
+            </div>
+            <div><Botao texto={t("Entre em contato")} rota= "https://wa.me/+5521999929790"></Botao></div>
         </Base>
     
     )
