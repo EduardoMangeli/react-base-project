ae5cca57199ef8599cf90b4efa8238d480aad25b
Ewerton Peixoto de Alencar Arrais <142873014+ewerside@users.noreply.github.com>
2024-05-16 14:21:33 -0300

    Formatando número do whatsapp automaticamente

--- src/pages/Guia.jsx
+++ src/pages/Guia.jsx
@@ -13,7 +13,7 @@ const Guia = () => {
                 <p>{t('Você é um guia turístico apaixonado por compartilhar os segredos e maravilhas da nossa cidade? Junte-se a nós e torne-se um parceiro em nossa plataforma de turismo! Junte-se a nós e faça parte de uma comunidade de guias apaixonados por turismo, compartilhando seu conhecimento e entusiasmo com viajantes de todo o mundo! Registre-se agora e comece a transformar sua paixão em uma experiência única de turismo!')}</p>
             </div>
             <div>
-                <Botao texto={t("Entre em contato")} isWhatsApp={true} numeroCelular={"5521999929790"} mensagem={"Olá! Estou interessado(a) em me tornar um guia turístico. Poderiam fornecer mais informações sobre como posso me cadastrar?"}></Botao>
+                <Botao texto={t("Entre em contato")} isWhatsApp={true} numeroCelular={"+55(21)99992-9790"} mensagem={"Olá! Estou interessado(a) em me tornar um guia turístico. Poderiam fornecer mais informações sobre como posso me cadastrar?"}></Botao>
             </div>
         </Base>
     );
