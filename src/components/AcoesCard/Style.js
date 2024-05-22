067f5537bd967b54d5aaac154d5eeb61cb7e7f16
André Costa Coelho <andreccoelho2004@gmail.com>
2024-05-22 15:31:52 -0300

    Adicionando animações ao dashboard.

--- src/components/AcoesCard/Style.js
+++ src/components/AcoesCard/Style.js
@@ -1,57 +1,67 @@
 import styled from "styled-components";
 
-
-
-
 const Card = styled.div`
+  display: flex;
+  margin: 20px;
+  padding: 10px;
+  min-width: 10%;
+  height: 60%;
+  background-color: var(--background);
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.171);
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);
+  }
+
+  &:active {
+    transform: scale(0.98);
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5);
+  }
+
+  #tipo {
     display: flex;
-    margin: 20px;
-    padding: 10px;
-    min-width: 25%;
-    height: 60%;
-    background-color: var(--background);
-    box-shadow: 00px 0px 10px rgba(0, 0, 0, 0.171);
-    border: 1 solid black;
+    flex-direction: column;
+    gap: 25%;
+    height: 100%;
+    font-family: 'Century Gothic', sans-serif;
+    font-weight: light;
+  }
 
-    #tipo {
-        display: flex;
-        flex-direction: column;
-        gap:25%;
-        height: 100%;
-        font-family: 'Century Gothic', sans-serif;
-        font-weight: light;
+  #tipo i {
+    font-size: 13px;
+  }
 
-    }
-    #tipo i {
-        font-size: 13px ;
-    }
-    #datadiv{
-        font-size:13px ;
-    }
-    .container-card{
-        display: flex;
-        gap: 0.2rem;
-        justify-content: left;
-        flex-direction: row;
-    .ID {
+  #datadiv {
+    font-size: 13px;
+  }
 
-        font-weight: bold;
-        color: var(--detalhes);
-        font-family: 'Century Gothic', sans-serif;
-        font-weight: light;
+  .container-card {
+    display: flex;
+    gap: 0.2rem;
+    justify-content: left;
+    flex-direction: row;
+  }
 
-    }
-    .icon{
-        width: 30px;
-        height: 30px;
-        border-radius: 50%;
-        text-align:center;
-        overflow: hidden; 
-        background-color: var(--botoes);
-        position: absolute;
-        margin-top:115px ;
-        margin-left:125px ;
-    }
+  .ID {
+    font-weight: bold;
+    color: var(--detalhes);
+    font-family: 'Century Gothic', sans-serif;
+    font-weight: light;
+  }
+
+  .icon {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    text-align: center;
+    overflow: hidden;
+    background-color: var(--botoes);
+    position: absolute;
+    margin-top: 105px;
+    margin-left: 125px;
+  }
 `;
 
-export {Card};
+export { Card };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,127 +1,107 @@
+import { useState } from "react";
 import Base from "./Base";
 import GraficoCode from "../components/Grafico/Grafico";
-import { useState } from "react";
 import ServicesBar from "../components/ServicesBar/ServicesBar.jsx";
-
-// AQUI IMPORTA OS DADOS DO JSON
-import dadosSolicitacoes from "../data/solicitacoes.json";
-import  AcoesCard  from "../components/AcoesCard/AcoesCard";
+import AcoesCard from "../components/AcoesCard/AcoesCard";
 import UsuariosCard from "../components/UsuariosCard/UsuarioCard";
-import dadosUsuarios from "../data/usuarios.json";
 import AutenticaçãoCard from "../components/AutenticaçãoCard/AutenticaçãoCard";
-import dadosAutenticação from "../data/autenticacao.json";
 import NotificacaoCard from "../components/NotificacaoCard/NotificacaoCard";
+
+import dadosSolicitacoes from "../data/solicitacoes.json";
+import dadosUsuarios from "../data/usuarios.json";
+import dadosAutenticação from "../data/autenticacao.json";
 import dadosNotificacao from "../data/notificacao.json";
 
 export const Home = () => {
-  /* useEffect(()=> {
-    onAuthStateChanged(auth, (user)=> {
-      if (user) {
-        window.sessionStorage.setItem("accessToken", user.accessToken);
-      } else {
-        window.sessionStorage.removeItem("accessToken");
-      }
-    })
-  },[]) */
-
-// AQUI VOCE CRIA O ESTADO QUE ARMAZENA OS DADOS 
-const [solicitacoes , setSolicitacoes] = useState(dadosSolicitacoes);
-const[usuarios, setUsuarios] = useState(dadosUsuarios);
-const[autenticacao, setAutenticação] = useState(dadosAutenticação)
-const[notificacao, setNotificacao] = useState(dadosNotificacao)
-const [selectedService, setSelectedService] = useState('Dosimetria Clínica');
-
+  // Estado para armazenar os dados
+  const [solicitacoes, setSolicitacoes] = useState(dadosSolicitacoes);
+  const [usuarios, setUsuarios] = useState(dadosUsuarios);
+  const [autenticacao, setAutenticação] = useState(dadosAutenticação);
+  const [notificacao, setNotificacao] = useState(dadosNotificacao);
+  const [selectedService, setSelectedService] = useState('Dosimetria Clínica');
 
   return (
     <Base>
       <h3>Dashboard |</h3>
-      <div class="dashboardFundo">
-        <div class="acoeRecentesContainer">
+      <div className="dashboardFundo">
+        <div className="acoeRecentesContainer">
           <h4>Ações Recentes:</h4>
-          <div class="linhaAcoes">
-          {solicitacoes.map(
-                                     (solicitacao) => (
-                                         <AcoesCard
-                                             key={solicitacao.id}
-                                               id={solicitacao.id}
-                                               status={solicitacao.status}
-                                               tempo={solicitacao.tempo}
-                                         />
-                                     )
-                                 )}
-                                
-                                 </div>
+          <div className="linhaAcoes">
+            {solicitacoes.map((solicitacao) => (
+              <AcoesCard
+                key={solicitacao.id}
+                id={solicitacao.id}
+                status={solicitacao.status}
+                tempo={solicitacao.tempo}
+              />
+            ))}
+          </div>
         </div>
+        
         <div className="container-234">
-          <div class="servicosContainer">
+          <div className="servicosContainer">
             <div className="grafico-area">
               <form id="servicos-options">
-                <label for="formulario">Serviços:</label>
+                <label htmlFor="formulario">Serviços:</label>
                 <select id="form-style" name="formulario">
                   <option value="dosimetriaClinica">Dosimetria Clinica</option>
                   <option value="dosimetriaPreClinica">Dosimetria Pré Clinica</option>
                   <option value="radiosinoviortese">Radiosinoviortese</option>
-                  <option value="segmentacao">Segementação</option>
+                  <option value="segmentacao">Segmentação</option>
                 </select>
               </form>
-              <GraficoCode></GraficoCode>
+              <GraficoCode />
             </div>
           </div>
+
           <div className="container-34">
-            <div class="usuarioContainer">
+            <div className="usuarioContainer">
               <h4>Usuários:</h4>
-              <div class="linhaUsuarios">
-          {usuarios.map(
-                                     (usuarios) => (
-                                         <UsuariosCard
-                                             key={usuarios.id}
-                                               id={usuarios.id}
-                                               Descrição={usuarios.Descrição}
-                                               Cargo={usuarios.Cargo}
-                                         />
-                                     )
-                                 )}
-                                
-                                 </div>
+              <div className="linhaUsuarios">
+                {usuarios.map((usuario) => (
+                  <UsuariosCard
+                    key={usuario.id}
+                    id={usuario.id}
+                    Descrição={usuario.Descrição}
+                    Cargo={usuario.Cargo}
+                  />
+                ))}
+              </div>
             </div>
-            <div class="autenticacaoContainer">
+
+            <div className="autenticacaoContainer">
               <h4>Autenticação e Autorização:</h4>
-              <div class="linhaAutenticação">
-              {autenticacao.map(
-                                     (autenticacao) => (
-                                         <AutenticaçãoCard
-                                             key={autenticacao.id}
-                                               id={autenticacao.id}
-                                               Descrição={autenticacao.Descrição}
-                                               Cargo={autenticacao.Membros}
-                                         />
-                                     )
-                                 )}
-                                
-                                 </div>
+              <div className="linhaAutenticação">
+                {autenticacao.map((autenticacao) => (
+                  <AutenticaçãoCard
+                    key={autenticacao.id}
+                    id={autenticacao.id}
+                    Descrição={autenticacao.Descrição}
+                    Cargo={autenticacao.Membros}
+                  />
+                ))}
+              </div>
             </div>
           </div>
         </div>
-        <div class="notificacaoContainer">
+
+        <div className="notificacaoContainer">
           <h4>Notificações:</h4>
           <ServicesBar onServiceChange={setSelectedService} />
-          <div class="linhaNotificacao">
-          {notificacao.map(
-                                     (notificacao) => (
-                                         <NotificacaoCard
-                                             key={notificacao.id}
-                                               id={notificacao.id}
-                                               Descrição={notificacao.Descrição}
-                                               Cargo={notificacao.Cargo}
-                                         />
-                                     )
-                                 )}
-                                
-                                 </div>
+          <div className="linhaNotificacao">
+            {notificacao.map((notificacao) => (
+              <NotificacaoCard
+                key={notificacao.id}
+                id={notificacao.id}
+                Descrição={notificacao.Descrição}
+                Cargo={notificacao.Cargo}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </Base>
   );
 };
 
-export default Home
+export default Home;
