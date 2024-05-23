d1594f75e84db54a187fc14d14aeb92784b7f281
Ewerton Peixoto de Alencar Arrais <142873014+ewerside@users.noreply.github.com>
2024-05-23 15:52:07 -0300

    Alinhamento geral da página home
    Imagens e textos entre o header e o footer. Upscale da imagem.

--- src/components/SectionsApp/SectionsApp.jsx
+++ src/components/SectionsApp/SectionsApp.jsx
@@ -7,23 +7,30 @@ const SectionsApp = ({ titulo, texto, imagem, link, align }) => {
         <Section align={align}>
         {align === "left" ? (
           <>
-            <div className="texto">
-              <h2>{titulo}</h2>
-              <p>{texto}</p>
-            </div>
-            <Link to={link}>
-              <img src={`${process.env.PUBLIC_URL}${imagem}`} />
-            </Link>
+            {/* <div className="leftAligned"> */}
+              <Link to={link} className="hiddenImage">
+                <img src={`${process.env.PUBLIC_URL}${imagem}`} />
+              </Link>
+              <div className="texto">
+                <h2>{titulo}</h2>
+                <p>{texto}</p>
+              </div>
+              <Link to={link} className="standardImage">
+                <img src={`${process.env.PUBLIC_URL}${imagem}`} />
+              </Link>
+            {/* </div> */}
           </>
         ) : (
           <>
-            <Link to={link}>
-              <img src={`${process.env.PUBLIC_URL}${imagem}`} />
-            </Link>
-            <div className="texto">
-              <h2>{titulo}</h2>
-              <p>{texto}</p>
-            </div>
+            {/* <div className="rightAligned"> */}
+              <Link to={link}>
+                <img src={`${process.env.PUBLIC_URL}${imagem}`} />
+              </Link>
+              <div className="texto">
+                <h2>{titulo}</h2>
+                <p>{texto}</p>
+              </div>
+            {/* </div> */}
           </>
         )}
     </Section>
