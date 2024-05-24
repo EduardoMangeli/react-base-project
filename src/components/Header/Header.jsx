0aee86adcdd84cb8f59887fd245cf574741e78b2
Zecademonho <jg@assis.net.br>
2024-05-24 19:03:52 -0300

    Novo Header, com os links somente para as paginas about, anuncie e guia; e novo botao para traducao

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,53 +1,96 @@
 import { Link } from "react-router-dom";
-import { Top } from "./Style";
+import { Top, MenuStyled, MenuContent, MenuIcon, MenuButton } from "./Style";
 import { slide as Menu } from 'react-burger-menu'
+import { MdOutlineTranslate } from 'react-icons/md';
 
-import i18n from "../../utils/i18n";
-import { useState } from "react";
+import { useTranslation } from 'react-i18next';
+
+import { useState, useEffect, useRef, useContext } from "react";
+
+const ROUTES = {
+  HOME: "/",
+  ABOUT: "/about",
+  ADVERTISE: "/anuncie",
+  GUIDE: "/guia",
+};
 
 const Header = () => {
+  const { i18n } = useTranslation();
   const [currentLanguage, setCurrentLanguage] = useState('pt')
+  const [menuVisible, setMenuVisible] = useState(false)
+  const toggleMenu = () => setMenuVisible(!menuVisible)
+  const menuRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setMenuVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const getLanguageName = (locale) => {
+    switch (locale) {
+      case 'en': return 'English';
+      case 'pt': return 'Português';
+      default: return '';
+    }
+  };
 
   const changeLanguage = value => {
     i18n.changeLanguage(value)
-    .then( () => {setCurrentLanguage(value)})
+    .then( () => {
+      setCurrentLanguage(value);
+    })
   }
 
   return (
     <Top>
     <div id="outer-container">
       <Menu outerContainerId={ "outer-container" } right noOverlay>
-        <Link to="/">Home</Link>
-        <Link to="/passeios">Passeios</Link>
+        <Link to={ROUTES.HOME}>Home</Link>
+        {/*<Link to="/passeios">Passeios</Link>
         <Link to="/gameficacao">Gameficação</Link>
-        <Link to="/mapa">Mapa</Link>
-        <Link to="/about">Sobre Nós</Link>
-        <Link to="/anuncie">Anuncie</Link>
-        <Link to="/guia">Seja um Guia</Link>  
+        <Link to="/mapa">Mapa</Link>*/}
+        <Link to={ROUTES.ABOUT}>Sobre Nós</Link>
+        <Link to={ROUTES.ADVERTISE}>Anuncie</Link>
+        <Link to={ROUTES.GUIDE}>Seja um Guia</Link>  
       </Menu>
     </div>
     <div className="divGeral">
-      <Link to="/" className="containerLogo">
+      <Link to={ROUTES.HOME} className="containerLogo">
         <img src="imagens/PasseioCariocaLogoBranco.svg" alt="Passeio Carioca" />
         <h3>Passeio<br/>Carioca</h3>
       </Link>
-      <div className="botoes">
-        <button className="botaoLng" onClick={() => changeLanguage('pt')} style={{ textDecoration: currentLanguage === 'pt' ? 'underline' : 'none' }}>
-          <img className="bandeira" src="imagens/bandeiras/Bandeira_do_Brasil.svg" />
-        </button>
-        /
-        <button className="botaoLng" onClick={() => changeLanguage('en')} style={{ textDecoration: currentLanguage === 'en' ? 'underline' : 'none' }}>
-          <img className="bandeira" src="imagens/bandeiras/Bandeira_do_USA.svg" alt="Inglês" />
-        </button>
-      </div>
       <div className="links">
-        <Link to="/" className="linkpages">Home</Link>
-        <Link to="/passeios" className="linkpages">Passeios</Link>
-        <Link to="/gameficacao" className="linkpages">Gameficação</Link>
-        <Link to="/mapa" className="linkpages">Mapa</Link>
-        <Link to="/about" className="linkpages">Sobre Nós</Link>
-        <Link to="/anuncie" className="linkpages">Anuncie</Link>
-        <Link to="/guia" className="linkpages">Seja um Guia</Link>
+        <Link to={ROUTES.HOME} className="linkpages">Home</Link>
+        {/*<Link to="/passeios">Passeios</Link>
+        <Link to="/gameficacao">Gameficação</Link>
+        <Link to="/mapa">Mapa</Link>*/}
+        <Link to={ROUTES.ABOUT}className="linkpages">Sobre Nós</Link>
+        <Link to={ROUTES.ADVERTISE} className="linkpages">Anuncie</Link>
+        <Link to={ROUTES.GUIDE} className="linkpages">Seja um Guia</Link>
+      </div>
+
+      <div ref={menuRef}>
+        <MenuButton id="menu-btn" type="checkbox" checked={menuVisible} onChange={toggleMenu}/>
+        <MenuStyled>
+          <MenuIcon htmlFor="menu-btn">
+            <MdOutlineTranslate />
+          </MenuIcon>
+          <span>{getLanguageName(i18n.language)}</span>
+        </MenuStyled>
+        <MenuContent isVisible= {menuVisible}>
+          <div className="botoes">
+            <button className="botaoLng" onClick={() => changeLanguage('pt')}>Português</button>
+            <button className="botaoLng" onClick={() => changeLanguage('en')}>English</button>
+          </div>
+        </MenuContent>
       </div>
     </div>
   </Top>
--- src/components/Header/Style.js
+++ src/components/Header/Style.js
@@ -7,6 +7,7 @@ const Top = styled.header`
   display: flex;
   align-items: center;
   flex-direction: row;
+  justify-content: space-between;
   margin: 0;
   height: auto;
   position: absolute;
@@ -30,28 +31,10 @@ const Top = styled.header`
   font-style: normal;
   }
 
-  .botoes{
-    color: var(--destaque);
-    display: flex;
-    position: fixed;
-    right: 28px;
-    top: 5px;
-    font-weight: bold;
-    z-index: 999;
-  }
   .links{
     display: none;
     flex-direction: row;
     /* flex-wrap: wrap; */
-  }
-
-  .botaoLng{
-    border: none;
-    background: none;
-    font-family: 'Roboto', sans-serif;
-    &:hover{
-      cursor: pointer;
-    }
   }
 
   a {
@@ -102,16 +85,6 @@ const Top = styled.header`
     }
   }
 
-  .bandeira{
-    display: block;
-    top: 2px;
-    left: 2px;
-    width: 20px;
-    height: 15px;
-    object-fit: cover;
-    align-items: center;
-    }
-
   @media screen and (min-width: 1024px){
     padding: 3.5em 0em;
     height: 10vh;
@@ -138,12 +111,6 @@ const Top = styled.header`
       display: none
     }
 
-    .botoes{
-      display: flex;
-      position: absolute;
-      top: 2px;
-      right: 2px;
-    }
     .divGeral{
       display: flex;
       justify-content: center;
@@ -154,4 +121,75 @@ const Top = styled.header`
   }
 `;
 
-export { Top };
+const MenuButton = styled.input`
+  display: none;
+`;
+
+const MenuStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--destaque);
+  gap: 10px;
+`;
+
+const MenuIcon = styled.label`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: .5rem;
+  border: 2px solid var(--destaque);
+  border-radius: 5px;
+  background-color: none;
+  &:hover {
+    border: 2px solid var(--secundaria);
+  }
+  cursor: pointer;
+  color: var(--destaque);
+`;
+
+const MenuContent = styled.div`
+  display: none;
+  flex-direction: column;
+  position: absolute;
+  top: 130px;
+  z-index: 999;
+
+  .botoes{
+    display: flex;
+    flex-direction: column;
+    width: fit-content;
+    height: fit-content;
+    gap: 1em;
+    padding: 1em;
+    background-color: var(--vidro);
+    border: 2px solid var(--destaque);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+  }
+
+  .botaoLng{
+    border: none;
+    padding: 1em;
+    padding-inline: 2em;
+    background: none;
+    font-family: 'Roboto', sans-serif;
+    color: var(--secundaria);
+    background: none;
+    &:hover{
+      color: var(--primaria);
+      cursor: pointer;
+    }
+  }
+
+  @media screen and (min-width: 1024px){
+    .botoes{
+      display: flex;
+      position: absolute;
+    }
+  }
+
+  ${({ isVisible }) => isVisible && `display: flex;`}
+`;
+
+export { Top, MenuStyled, MenuIcon, MenuContent, MenuButton};
