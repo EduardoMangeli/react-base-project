97562cb334c7c23acb10644fb316c0012c3b869d
Zecademonho <jg@assis.net.br>
2024-05-24 20:25:06 -0300

    Revert "arrmando traducao e adicionando mais um titulo para a section app"
    This reverts commit 969d23a4701db4afa33f561e71b60cf5a8b6e548.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -63,7 +63,6 @@ const Home = () => {
 
       <h2 className="texto-destaque">{t('Conheça a Passeio Carioca')}</h2>
 
-      <h2 className="titulo-destaque">{t('Passeios Guiados')}</h2>
       <SectionsApp 
       titulo={t("Passeios guiados")}
       texto={t("Explore a cidade com nossos passeios guiados personalizados. Nossos guias turísticos experientes o levarão a uma jornada única, compartilhando histórias fascinantes e segredos bem guardados dos pontos turísticos mais icônicos da cidade.")}
@@ -92,7 +91,7 @@ const Home = () => {
           ))}
         </Swiper>
 
-      <h2 className="titulo-destaque">{t('Gameficação do aplicativo')}</h2>
+      <h2 className="texto-destaque">{t('Gameficação do aplicativo')}</h2>
       <SectionsApp 
       titulo={t("Gameficação")}
       texto={t("Transforme sua experiência de turismo em uma aventura emocionante! Explore os pontos turísticos incríveis da cidade e colecione medalhas digitais ao longo do caminho. Com nossa funcionalidade de gamificação, cada lugar que você visita se torna uma conquista única. Tire uma foto, marque sua presença no aplicativo e ganhe suas medalhas. Quanto mais lugares você visitar, mais medalhas você ganha!")}
@@ -100,7 +99,7 @@ const Home = () => {
       align="left"
       link={"/gameficacao"}/>
 
-      <h2 className="titulo-destaque">{t('Mapa da Passeio Carioca')}</h2>
+      <h2 className="texto-destaque">{t('Mapa da Passeio Carioca')}</h2>
       <SectionsApp 
       titulo={t("Mapa Geolocalizado")}
       texto={t("Descubra a cidade como nunca antes com nosso mapa geolocalizado. Explore pontos de interesse ao redor de você e mergulhe na história de cada local. Clique nos pins do mapa para ler sobre a história do local, curiosidades interessantes e dicas exclusivas de nossos especialistas locais.")}
